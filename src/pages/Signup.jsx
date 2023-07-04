b7298fa6898a12489853e22154aad8cb56a0121f
justina177 <justinaenenche@yahoo.com>
2023-07-04 12:48:43 +0100

    added function to handle sigup using firebase

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import BackgroundImage from '../Components/BackgroundImage';
 import Header from '../Components/Header';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { firebaseAuth } from '../utils/firebase-config'
 
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -12,8 +14,16 @@ const Signup = () => {
   });
 
   const handleSignIn = async () => {
-    console.log (formValues);
+    try {
+      const { email, password } = formValues;
+      await createUserWithEmailAndPassword(firebaseAuth, email, password)
+    } catch (err){
+      console.log(err)
+    }
   }
+  
+  
+
   return (
     <Container>
         <BackgroundImage />
--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,18 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
+import { initializeApp } from "firebase/app";
+import { getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBtLD5tXK2N-0O8e_cEDGhDincOtkqd6vQ",
-    authDomain: "react-netflix-clone-b98d4.firebaseapp.com",
-    projectId: "react-netflix-clone-b98d4",
-    storageBucket: "react-netflix-clone-b98d4.appspot.com",
-    messagingSenderId: "515738692247",
-    appId: "1:515738692247:web:ec4fe14579b0d7695197d2",
-    measurementId: "G-WP78XRWDLZ"
-  };
-  
- 
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyBtLD5tXK2N-0O8e_cEDGhDincOtkqd6vQ",
+  authDomain: "react-netflix-clone-b98d4.firebaseapp.com",
+  projectId: "react-netflix-clone-b98d4",
+  storageBucket: "react-netflix-clone-b98d4.appspot.com",
+  messagingSenderId: "515738692247",
+  appId: "1:515738692247:web:ec4fe14579b0d7695197d2",
+  measurementId: "G-WP78XRWDLZ"
+};
 
-  export const firebaseAuth = getAuth(app);
+
+const app = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(app);
+
+
